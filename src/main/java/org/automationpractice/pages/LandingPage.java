5445e6392cda81ee67970a498924f2b478f7d794
Michal Kasiarz <michal.kasiarz@protonmail.com>
2020-09-11 18:08:09 +0200

    LandingPage added

---
+++ src/main/java/org/automationpractice/pages/LandingPage.java
@@ -0,0 +1,20 @@
+package org.automationpractice.pages;
+
+import org.automationpractice.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LandingPage extends BasePage {
+
+    @FindBy(css = "a.login")
+    private WebElement buttonSignIn;
+
+    public LandingPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickButtonSignIn() {
+        buttonSignIn.click();
+    }
+}
